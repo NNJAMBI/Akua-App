554411f41c8fe5702d949862cb7210a4de235906
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-05-28 23:45:21 +0300

    add arrays for days of the week, male Akan names and female Akana names

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
