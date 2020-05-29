1e52e011c47b639e30ba8c21066090bcb8c51df7
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-05-29 22:45:30 +0300

    add js functions for the submit buttion and vdecalred variables for date of birth provided in the form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-
+var submit = document.getElementById("submit");
+submit.addEventListener(click, function(){  
+}
 
 var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+var dd = document.getElementById("day").value;
+var mm = document.getElementById("month").value;
+var yyyy = document.getElementById("year").value;
