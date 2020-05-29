a9f81ee53f241324a0e1513641a9d22c9e83c977
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-05-29 23:22:57 +0300

    add changes to the js file after refactoring the code which was not responsive

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
 var submit = document.getElementById("submit");
-submit.addEventListener(click, function(){  
-}
 
-var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+submit.addEventListener("click", function() {
+  var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var dd = document.getElementById("day").value;
-var mm = document.getElementById("month").value;
-var yy = document.getElementById("year").value;
-var date = new Date(yy + "_" + mm + "_" + dd);
-var daysOfTheWeek = date.getDay();
-var male = document.getElementById("male");
-var female = document.getElementById("female");
-var output = document.getElementById("output");
-var name = document.getElementById("name").value;
 
-if (dd <1 || dd > 31) {
+  var dd = document.getElementById("day").value;
+  var mm = document.getElementById("month").value;
+  var yy = document.getElementById("year").value;
+  var date = new Date(yy + "-" + mm + "-" + dd);
+  var daysOfWeek = date.getDay();
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
+  var output = document.getElementById("output");
+  var name = document.getElementById("name").value;
+
+  if (dd < 1 || dd > 31) {
     alert("Please input a valid day!");
-}
-else if (mm < 1 || mm >12) {
-alert("Please enter a valid month!");
-}
-else if (yy.toString().length !== 4) {
-    alert("Please enter a valid year!");
-}
-else if (male.checked) {
+  } else if (mm < 1 || mm > 12) {
+    alert("please enter a valid month!");
+  } else if (yy.toString().length !== 4) {
+    alert("please enter a valid year!");
+  } else if (male.checked) {
     alert("Awesome!.." + " " + name + " " + "your Akan name is" + " " + maleAkanNames[daysOfWeek] + " " + "you were born on" + " " + daysOfTheWeek[daysOfWeek]);
-} else {
-  alert("Awesome!.." + " " + name + " " + "your Akan name is" + " " + femaleAkanNames[daysOfWeek] + " " + "you were born on" + " " + daysOfTheWeek[daysOfWeek]);
-}
+  } else {
+    alert("Awesome!.." + " " + name + " " + "your Akan name is" + " " + femaleAkanNames[daysOfWeek] + " " + "you were born on" + " " + daysOfTheWeek[daysOfWeek]);
+  }
 })
